fb8ecfd2bfe770750a54ec4325672f194172fbb3
Wieland Hoffmann <themineo@gmail.com>
2014-07-14 11:53:20 +0200

    Reexport uuid.Parse

---
+++ reexports.go
@@ -0,0 +1,12 @@
+package caa
+
+import (
+	u "code.google.com/p/go-uuid/uuid"
+)
+
+// StringToUUID is a reexported helper function of the UUID module to parse a
+// string into a UUID.
+func StringToUUID(str string) (uuid u.UUID) {
+	uuid = u.Parse(str)
+	return
+}
